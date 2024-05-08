d2e7eaa98ce3d9b8ebe1eca154a31bce98017780
joramgranger@outlook.com <joramgranger@outlook.com>
2024-05-07 18:56:15 -0700

    crud

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 
 dotenv.config();
 
@@ -16,6 +18,8 @@ mongoose.connect(process.env.MONGO_URL)
     app.use(express.json());
     app.use("/api/auth", authRoute);
     app.use("/api/users", userRoute);
+    app.use("/api/products", productRoute);
+    app.use("/api/carts", cartRoute);
 
 
 app.listen(process.env.PORT || 5000, ()=> {
