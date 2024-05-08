7ff75b93c194621aa901ec823860aa428527f624
joramgranger@outlook.com <joramgranger@outlook.com>
2024-05-07 17:20:48 -0700

    Auth

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,63 @@
+const CryptoJS = require("crypto-js");
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-    res.send("Welcome new user");
-});  
-
-
-router.post("/userposttest", (req, res) => {
-  const username = req.body.username;
-  console.log(username);
-  res.send("your username is" + username);
+// UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if(req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }  
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+      $set: req.body
+    }, {new:true}
+  );
+  res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-/*
-//routers/blogPostsRouter.js
-router.get('/', (req, res) => {
-  res.json(BlogPosts.get());
-});
-*/
-module.exports = router
+// DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).json("User has been deleted...")
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// GET
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    const { password, ...others} = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// GET ALL USERS
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new
+  try {
+    const users = query 
+    ? await User.find().sort({ _id: -1}).limit(1) 
+    : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// GET USER STATS
+
+
+module.exports = router;
